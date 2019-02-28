44df8edb931f9b6ca15d6d20de3adbedc018f48c
Siarhei Danilovich <Siarhei_Danilovich@epam.com>
2019-02-28 19:28:18 +0300

    + Added base site (categories&products pages), configured startup and configurations

---
+++ Northwind.Web/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Northwind.DataAccess.Context;
+using Northwind.DataAccess.Models;
+using Northwind.Web.Helpers;
+using Northwind.Web.Middleware;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Northwind.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        protected readonly NorthwindContext context;
+        public PageNumberService service;
+        public CategoryController(NorthwindContext context, PageNumberService service)
+        {
+            this.context = context;
+            this.service = service;
+        }
+        // GET: /<controller>/
+        public async Task<IActionResult> Index(int? page)
+        {
+            return View(await PaginatedList<Categories>.CreateAsync(context.Categories.AsNoTracking(), page ?? 1, service.pageSize));
+        }
+    }
+}
---
+++ Northwind.Web/Middleware/PageNumberMiddleware.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Northwind.Web.Middleware
+{
+    public class PageNumberService
+    {
+        public short pageSize;
+        public PageNumberService(IConfiguration config)
+        {
+            pageSize = Int16.Parse(config.GetSection("section0:M").Value);
+        }
+    }
+}
