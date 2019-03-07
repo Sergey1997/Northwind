8b62881b3e85e58a2c474325c7a381ac8cf7eaa6
Sergey Danilovich <danilovichser@gmail.com>
2019-03-08 00:11:12 +0300

    * Some fixes with views

--- Northwind.Web/Startup.cs
+++ Northwind.Web/Startup.cs
@@ -23,8 +23,7 @@ namespace Northwind.Web
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
             string connection = "NorthwindConnection";
@@ -36,7 +35,6 @@ namespace Northwind.Web
 
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
@@ -44,8 +42,7 @@ namespace Northwind.Web
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -55,9 +52,11 @@ namespace Northwind.Web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
                 app.UseHsts();
             }
+
+            app.UseStatusCodePages("text/plain", "Status code page, status code: {0}");
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
